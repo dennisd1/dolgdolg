e379a78623521d39d2b30f877506c954fd3fc926
dennisd1 <mbl.acc4@gmail.com>
2017-04-24 17:49:58 +0300

    Update loader.js

--- a/loader.js
+++ a/loader.js
@@ -1,1 +1,16 @@
-<
+	var url;
+	var id;
+	var j=1;
+	
+$(document).ready(function () {
+	$('div[data-loader]').each(function() {
+		url=$(this).attr("data-loader");
+		id=$(this).attr("id");
+    		var tmp_j = j++;
+    		 $.get(url, function(data){
+			 $('#'+tmp_j).html(data);
+	
+			 console.log("# increased"+tmp_j);
+			 });
+	});
+});

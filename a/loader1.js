32ca76645ae65ed566457984a8ead47277961e2f
dennisd1 <mbl.acc4@gmail.com>
2017-05-13 17:14:06 +0300

    Update loader1.js

--- a/loader1.js
+++ a/loader1.js
@@ -35,7 +35,7 @@ function clickfunction(){
 				
 				$(this).closest(".b-table-row").find('a[href*="#side/info"]');     //!!!! DOESNT WORK
 				
-			        console.log("RAW = ", $(this).closest(".b-table-row").html());
+			        console.log("RAW = ", $(this).closest(".b-table-row").find('a[href*="#side/info"]').filter(':first'));
 			}
 
 		});

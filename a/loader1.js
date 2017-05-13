30d4f0c4b53a98405e90022b8f5e25c2ef12a8fa
dennisd1 <mbl.acc4@gmail.com>
2017-05-13 17:50:14 +0300

    Update loader1.js

--- a/loader1.js
+++ a/loader1.js
@@ -35,8 +35,8 @@ function clickfunction(){
 				
 				$(this).closest(".b-table-row").find('a[href*="#side/info"]');     //!!!! DOESNT WORK
 				
-			        console.log("RAW = ", $(this).closest(".b-table-row").find('a:eq( 2 )').html());
-				 //console.log("RAW = ", $(this).closest(".b-table-row").find('a:eq( 2 )').filter(':last').html());
+			        console.log("RAW = ", $(this).closest(".b-table-row").find('a'));
+				 //console.log("RAW = ", $(this).closest(".b-table-row").find('a').filter(':last').html());
 			}
 
 		});
